0cf08afb951e7b052e3dd38f7e501af59f18c362
Saurabh Lodha <slodha@techracers.io>
2019-08-28 14:14:50 +0530

    Train Search Api Done

--- controllers/trainStatus/trainsBetween.js
+++ controllers/trainStatus/trainsBetween.js
@@ -5,9 +5,9 @@ const Op = db.Sequelize.Op;
 import { getCurrCity } from './fillStations';
 import { reservationsUrl } from 'twilio/lib/jwt/taskrouter/util';
 
-export async function trainBetween(req, res) {
-    const sCity = req.body.sCity;
-    const dCity = req.body.dCity;
+export async function trainBetween(sCity, dCity) {
+    // const sCity = req.body.sCity;
+    // const dCity = req.body.dCity;
     let x = moment().format();
     x = x.split('T');
     let time = x[0] + " " + x[1];
@@ -29,15 +29,17 @@ export async function trainBetween(req, res) {
     for (let i = 0; i < answer.length; i++) {
         let trainId = answer[i];
         let temp1 = await models.trainStatuses.findAll({ where: { sTime: { [Op.gte]: time }, isRunning: true, sCity: sCity, trainId: trainId } });
-        let temp2 = await models.trainStatuses.findAll({ where: { sTime: { [Op.gte]: time }, isRunning: true, dCity: dCity, trainId: trainId } });
+        let temp2 = await models.trainStatuses.findAll({ where: { sTime: { [Op.gte]: time }, isRunning: true, sCity: dCity, trainId: trainId } });
         for(let j=0; j<temp1.length; j++){
         final_answer.push({ trainId:trainId, sourceStatusId: temp1[j].dataValues.id, destinationStatusId: temp2[j].dataValues.id });
         }
     }
     if(final_answer.length == 0){
-    console.log('No trains between stations');
+    let msg = 'No trains between stations';
+    return msg;
+    //change here for 0;
     }
     else{
-        res.send(final_answer);
+        return final_answer;
     }
 }
