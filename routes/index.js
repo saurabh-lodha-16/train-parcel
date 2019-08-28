9bbb3977d684f84b78d09d8c5485cad0283b369e
Akshay Kumar Sharma <akzarma@gmail.com>
2019-08-28 23:10:54 +0530

    resend otp backend fix

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 import { loginGet, logoutGet } from '../controllers/auth/login'
 import { loginPost } from '../controllers/auth/login'
-import { registerGet } from '../controllers/auth/register'
+import { registerGet, resendOTPGet } from '../controllers/auth/register'
 import { registerPost } from '../controllers/auth/register'
 import { dashboardGet } from '../controllers/dashboard'
 import { renderChangePassword } from '../controllers/auth/changePassword'
@@ -21,5 +21,7 @@ router.get('/changePassword', renderChangePassword);
 router.post('/changePassword', changePassword);
 
 router.get('/dashboard', dashboardGet);
+
+router.get('/register/resendOTP/:userId', resendOTPGet)
 
 module.exports = router;
