f81a6f19433b8e5903420118b433fdf30ffd1908
Akash Pande <apande@techracers.io>
2019-08-29 14:49:15 +0530

    Added Change password Facilty

--- controllers/auth/changePassword.js
+++ controllers/auth/changePassword.js
@@ -1,6 +1,6 @@
 import db from '../../models';
-
-let users = db['users'];
+const bcrypt = require('bcrypt')
+const users = db['users'];
 
 function checkPassword(str) {
   let re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/;
@@ -10,7 +10,9 @@ function checkPassword(str) {
 export function renderChangePassword(req, res) {
   try {
     if (req.session.user) {
-      res.render('auth/changePassword.ejs');
+      res.render('base',{
+        content: 'auth/changePassword.ejs'
+      });
     } else {
       res.redirect('/login');
     }
@@ -21,8 +23,9 @@ export function renderChangePassword(req, res) {
 
 async function updatePassword(userId, password) {
   try {
+    let hashPwd = bcrypt.hashSync(password, 10);
     let updatedInstance = await users.update(
-      { password: password },
+      { password: hashPwd },
       { where: { id: userId } }
     );
   } catch (err) {
@@ -32,47 +35,65 @@ async function updatePassword(userId, password) {
 
 export async function changePassword(req, res) {
   try {
+
     let user = req.session.user;
     if (user) {
       let oldPassword = req.body.oldPassword;
       let newPassword = req.body.newPassword;
       let reNewPassword = req.body.reNewPassword;
+
       if (!(checkPassword(newPassword) && checkPassword(reNewPassword))) {
-        res.render('auth/changePassword.ejs', {
+        res.render('base', {
+          content: 'auth/changePassword.ejs',
           alertMsg: `Password should contain at least one lowercase letter, one uppercase letter,
           one number and 8 other letters.`,
           alert: "danger"
         });
       }
       if (!(oldPassword && newPassword && reNewPassword)) {
-        res.render('auth/changePassword.ejs', {
+        res.render('base', {
+          content: 'auth/changePassword.ejs',
           alertMsg: "One or more blank fields found.",
           alert: "danger"
         });
       }
+      if ((oldPassword === newPassword )) {
+        res.render('base', {
+          content: 'auth/changePassword.ejs',
+          alertMsg: "New Password must be different from Old Password.",
+          alert: "danger"
+        });
+      }
       if (newPassword !== reNewPassword) {
-        res.render('auth/changePassword.ejs', {
+        res.render('base', {
+          content: 'auth/changePassword.ejs',
           alertMsg: "Passwords didn't match",
           alert: "danger"
         });
       }
-      if (user.password === oldPassword) {
-        updatePassword(userId, newPassword);
-        res.render('auth/changePassword.ejs', {
-          alertMsg: "Password successfully updated.",
-          alert: "success"
-        });
-      } else {
-        res.render('auth/changePassword.ejs', {
-          alertMsg: "Old password is incorrect.",
-          alert: "danger"
-        });
-      }
+      await bcrypt.compare(oldPassword, user.password, async function (err, result) {
+        if (result) {
+          await updatePassword(user.id, newPassword);
+          res.render('base', {
+            content: 'auth/changePassword.ejs',
+            alertMsg: "Password successfully updated.",
+            alert: "success"
+          });
+
+        } else {
+          res.render('base', {
+            content: 'auth/changePassword.ejs',
+            alertMsg: "Old password is incorrect.",
+            alert: "danger"
+          });
+        }
+      })
     } else {
       res.redirect('/login');
     }
   } catch (err) {
-    res.render('auth/changePassword.ejs', {
+    res.render('base', {
+      content: 'auth/changePassword.ejs',
       alertMsg: err,
       alert: "danger"
     });
