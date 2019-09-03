04105057d4bc58edf2cd5e39dc1d26ae315eddbb
Akash Pande <apande@techracers.io>
2019-09-03 18:55:17 +0530

    Changes made in role

--- routes/role.js
+++ routes/role.js
@@ -1,11 +1,10 @@
-import { renderRolePage, addRole, editRole, addRoleResult, editRoleResult } from '../controllers/role/edit';
-import { viewUsers, editUserRole, editUserRoleResult } from '../controllers/userRole/edit'
+import { renderRolePage, addRole, editRole, addRoleResult, editRoleResult } from '../controllers/role/editRole';
 import express from 'express';
 let router = express.Router();
 router.get('/', renderRolePage);
 router.get('/add', addRole);
-router.post('/add', addRoleResult);
+router.post('/', addRoleResult);
 router.get('/edit', editRole);
-router.put('/edit', editRoleResult);
+router.put('/', editRoleResult);
 module.exports = router;
 
