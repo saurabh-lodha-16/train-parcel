92e01ba774695fcc169071d2a6be9754e488c1c1
lokesh435 <lpatil@techracers.io>
2019-09-03 18:29:05 +0530

    Renamed controller files and reorganized directory structure.

--- routes/role.js
+++ routes/role.js
@@ -1,5 +1,5 @@
-import { renderRolePage, addRole, editRole, addRoleResult, editRoleResult } from '../controllers/role/editRole';
-import { viewUsers, editUserRole, editUserRoleResult } from '../controllers/role/editUserRole'
+import { renderRolePage, addRole, editRole, addRoleResult, editRoleResult } from '../controllers/role/edit';
+import { viewUsers, editUserRole, editUserRoleResult } from '../controllers/userRole/edit'
 import express from 'express';
 let router = express.Router();
 router.get('/', renderRolePage);
