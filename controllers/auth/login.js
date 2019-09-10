ec1af0527c851fe01a64483450396a54508c323e
Akshay Kumar Sharma <akzarma@gmail.com>
2019-09-10 17:40:05 +0530

    indentation fix

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,56 +1,56 @@
 import * as models from '../../models'
 const bcrypt = require('bcrypt')
 export function loginGet(req, res) {
-	let user = req.session.user
-	if (!user) {
-		res.render('auth/login')
-	} else {
-		res.redirect('/dashboard')
-	}
+  let user = req.session.user
+  if (!user) {
+    res.render('auth/login')
+  } else {
+    res.redirect('/dashboard')
+  }
 }
 
 export function loginPost(req, res) {
-    let email = req.body.email
-    let pwd = req.body.pwd
-    try {
-        models.users.findOne({
-            where: {
-                email: email
-            }
-        }).then(user => {
-            if (user) {
-                bcrypt.compare(pwd, user.password, function (err, result) {
-                    if (result) {
-                        // Passwords match
-                        req.session.user = user
-                        res.redirect('/dashboard')
-                    } else {
-                        // Passwords don't match
-                        res.render('auth/login', { alert: 'danger', alertMsg: 'You have entered wrong credentials!' })
-                    }
-                })
-
-            } else {
-                res.render('auth/login', { alert: 'danger', alertMsg: 'You have entered wrong credentials!' })
-            }
+  let email = req.body.email
+  let pwd = req.body.pwd
+  try {
+    models.users.findOne({
+      where: {
+        email: email
+      }
+    }).then(user => {
+      if (user) {
+        bcrypt.compare(pwd, user.password, function (err, result) {
+          if (result) {
+            // Passwords match
+            req.session.user = user
+            res.redirect('/dashboard')
+          } else {
+            // Passwords don't match
+            res.render('auth/login', { alert: 'danger', alertMsg: 'You have entered wrong credentials!' })
+          }
         })
-    } catch (e) {
-        res.render('auth/login', { alert: 'danger', alertMsg: `Exception: ${e.message}` })
-    }
+
+      } else {
+        res.render('auth/login', { alert: 'danger', alertMsg: 'You have entered wrong credentials!' })
+      }
+    })
+  } catch (e) {
+    res.render('auth/login', { alert: 'danger', alertMsg: `Exception: ${e.message}` })
+  }
 
 }
 
 
 export function logoutGet(req, res) {
-	if (req.session) {
-		req.session.destroy((err) => {
-			if (err) {
-				res.write(err)
-				res.end()
-			} else {
-				res.redirect('/dashboard')
-			}
-		})
-	}
+  if (req.session) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.write(err)
+        res.end()
+      } else {
+        res.redirect('/dashboard')
+      }
+    })
+  }
 }
 
--- controllers/payment/index.js
+++ controllers/payment/index.js
@@ -4,57 +4,57 @@ import { sendWAmsg } from "../services/common"
 import { redirectWithMsg } from '../services/common'
 
 export async function makePayment(req, packageId, loggedUser, res) {
-	try {
-		let packageObj = await models.packages.findOne({
-			where: { id: packageId },
-			include: [
-				{ model: models.users }
-			]
-		})
-		if (packageObj) {
-			let amountToBeCharged = packageObj.weight * 100 * 100
-			stripe.customers
-				.create({
-					email: packageObj.user.email,
-					phone: packageObj.user.mobileNo
-				})
-				.then((customer) => {
-					return stripe.customers.createSource(customer.id, {
-						source: 'tok_visa',
-					})
-				})
-				.then((source) => {
-					return stripe.charges.create({
-						amount: amountToBeCharged,
-						currency: 'inr',
-						customer: source.customer,
-					})
-				})
-				.then(async (charge) => {
-					sendWAmsg(packageObj.user.mobileNo, `We have received a payment of ₹${amountToBeCharged / 100}\n\
+  try {
+    let packageObj = await models.packages.findOne({
+      where: { id: packageId },
+      include: [
+        { model: models.users }
+      ]
+    })
+    if (packageObj) {
+      let amountToBeCharged = packageObj.weight * 100 * 100
+      stripe.customers
+        .create({
+          email: packageObj.user.email,
+          phone: packageObj.user.mobileNo
+        })
+        .then((customer) => {
+          return stripe.customers.createSource(customer.id, {
+            source: 'tok_visa',
+          })
+        })
+        .then((source) => {
+          return stripe.charges.create({
+            amount: amountToBeCharged,
+            currency: 'inr',
+            customer: source.customer,
+          })
+        })
+        .then(async (charge) => {
+          sendWAmsg(packageObj.user.mobileNo, `We have received a payment of ₹${amountToBeCharged / 100}\n\
 					Package Serial ID: ${packageObj.serial_no}\nTransaction ID: ${charge.balance_transaction}`)
-					redirectWithMsg('/packages', req, res, 'success', `Payment has been processed successfully.\
+          redirectWithMsg('/packages', req, res, 'success', `Payment has been processed successfully.\
 					 Package Serial ID: ${packageObj.serial_no}. Transaction ID: ${charge.balance_transaction}`)
-				})
-				.catch(async (err) => {
-					await models.packages.update(
-						{ isActive: false },
-						{
-							where: { id: packageId }
-						})
-					redirectWithMsg('/packages', req, res, 'danger', `Payment Failed. Register again! ${err}`)
-				})
+        })
+        .catch(async (err) => {
+          await models.packages.update(
+            { isActive: false },
+            {
+              where: { id: packageId }
+            })
+          redirectWithMsg('/packages', req, res, 'danger', `Payment Failed. Register again! ${err}`)
+        })
 
-		} else {
-			redirectWithMsg('/packages', req, res, 'danger', `Some error has occured! No Package found while doing payment. Please Try again.`)
-		}
-	} catch (e) {
-		await models.packages.update(
-			{ isActive: false },
-			{
-				where: { id: packageId }
-			})
-		redirectWithMsg('/packages', req, res, 'danger', `Payment Failed. Please Try again. Exception: ${e}`)
-	}
+    } else {
+      redirectWithMsg('/packages', req, res, 'danger', `Some error has occured! No Package found while doing payment. Please Try again.`)
+    }
+  } catch (e) {
+    await models.packages.update(
+      { isActive: false },
+      {
+        where: { id: packageId }
+      })
+    redirectWithMsg('/packages', req, res, 'danger', `Payment Failed. Please Try again. Exception: ${e}`)
+  }
 }
 
